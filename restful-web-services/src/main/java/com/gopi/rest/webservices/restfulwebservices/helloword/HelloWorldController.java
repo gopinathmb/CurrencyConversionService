b11ae247aaf490d4c837853bb5f11cb54866886f
gopinathmb <gopinathjnv@gmail.com>
2022-03-20 13:10:28 +0530

    Supporting i18n for the application

--- restful-web-services/src/main/java/com/gopi/rest/webservices/restfulwebservices/helloword/HelloWorldController.java
+++ restful-web-services/src/main/java/com/gopi/rest/webservices/restfulwebservices/helloword/HelloWorldController.java
@@ -1,11 +1,17 @@
 package com.gopi.rest.webservices.restfulwebservices.helloword;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private MessageSource messageSource;
 
     @GetMapping("/hello-world")
     public String helloWorld() {
@@ -20,5 +26,12 @@ public class HelloWorldController {
     @GetMapping("/hello-world-path-variable/{name}")
     public HelloWorldBean getHelloWorldPathVariable(@PathVariable String name) {
         return new HelloWorldBean(String.format("Hello World through path variable. Hello, %s ", name));
+    }
+
+    @GetMapping("/hello-world-internationalized")
+    public String helloWorldInternationalized() {
+        System.out.println(LocaleContextHolder.getLocale());
+        return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
+
     }
 }
