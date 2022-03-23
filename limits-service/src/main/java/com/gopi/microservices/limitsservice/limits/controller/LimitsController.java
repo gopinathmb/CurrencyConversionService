b0d5e18f652616d3e8cfd166f5fb352495aa58ea
gopinathmb <gopinathjnv@gmail.com>
2022-03-23 06:08:44 +0530

    Refactoring limit service

--- limits-service/src/main/java/com/gopi/microservices/limitsservice/limits/controller/LimitsController.java
+++ limits-service/src/main/java/com/gopi/microservices/limitsservice/limits/controller/LimitsController.java
@@ -1,15 +1,20 @@
 package com.gopi.microservices.limitsservice.limits.controller;
 
 import com.gopi.microservices.limitsservice.limits.bean.Limits;
+import com.gopi.microservices.limitsservice.limits.configuration.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsController {
 
+    @Autowired
+    private Configuration configuration;
+
     @GetMapping("/limits")
     public Limits retriveLimits() {
-
-        return new Limits(1, 100);
+        return new Limits(configuration.getMinimum(), configuration.getMaximum());
+        // return new Limits(1, 100);
     }
 }
